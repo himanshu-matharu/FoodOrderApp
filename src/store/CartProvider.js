147a7e4fe33725365a09315feba77002b5a44fc9
Himanshu Matharu <89752238+himanshu-matharu@users.noreply.github.com>
2022-06-09 11:29:49 -0400

    - Added order submission http method

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,10 @@ const cartReducer = (state, action) => {
       updatedItems[existingCartItemIndex] = updatedItem;
     }
 
+    if (action.type === "CLEAR") {
+      return defaultCartState;
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
@@ -85,11 +89,16 @@ const CartProvider = (props) => {
     });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
